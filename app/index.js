92ae30108e275c75c154e75278fb4cb139a34f71
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-10 16:56:25 +0530

    Add ScrollView and SafeAreaView to Home component, and update imports in index.js*** ***Update ScreenHeaderBtn component to use TouchableOpacity and Image*** ***Update Welcome component to use useState, TextInput, TouchableOpacity, and Image

--- app/index.js
+++ app/index.js
@@ -1,11 +1,48 @@
-import {View,Text} from 'react-native';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useRouter, Stack } from 'expo-router';
 
-const Home =()=>{
-    return(
-        <View>
-            <Text>Home uu</Text>
-        </View>
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
+const Home = () => {
+
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightwhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <View>
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                        </View>
+                    ),
+                    headerRight: () => (
+                        <View>
+                            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                        </View>
+                    ),
+                    headerTitle: ""
+                }}
+            />
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View
+            style={{
+                            flex: 1,
+                            padding: SIZES.medium
+                        }}>
+                <Welcome />
+                <Popularjobs />
+                <Nearbyjobs />
+
+            </View>
+
+        </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
-export default Home
+export default Home;

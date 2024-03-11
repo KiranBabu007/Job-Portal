4557da2f15f59e336551cf43b77dd6cca0afeee4
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-11 23:56:24 +0530

    Add font loading and SplashScreen functionality to HomeLayout component and implement job type tabs in Welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -4,14 +4,58 @@ import {useRouter} from 'expo-router';
 import styles from './welcome.style';
 import {icons,SIZES} from '../../../constants';
 
+const jobTypes =["Full-time","Part-time","Remote"];
+
 const Welcome = () => {
   const router =useRouter();
+
+  const [activeJobType,setActiveJobType] = useState(jobTypes[0]);
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Hello Kiran</Text>
         <Text style={styles.welcomeMessage}>Find your Perfect Job</Text>
       </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+          style={styles.searchInput}
+          value=""
+          onChange={()=>{}}
+          placeholder='What are you looking for?'
+          />
+        </View>
+      <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+        <Image 
+        source={icons.search}
+        resizeMode='contain'
+        style={styles.searchBtnImage}
+        />
+      </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+        data={jobTypes}
+        renderItem={({item})=>(
+            <TouchableOpacity
+            style={styles.tab(activeJobType,item)}
+            onPress={()=>{
+              setActiveJobType(item);
+              router.push(`/search/${item}`)
+            }
+            }
+            >
+              <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+            </TouchableOpacity>
+        )}
+          keyExtractor={item=>item}
+
+        />
+
+      </View>
+
     </View>
   )
 }
